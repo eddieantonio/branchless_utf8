66d86a3d5500e384531e31ac9213c19d152c9ccc
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2025-01-26 17:50:24 +0000

    change: exact size allocations

--- src/implementations/scalar_branchless.rs
+++ src/implementations/scalar_branchless.rs
@@ -13,50 +13,88 @@ const SHIFT_LUT: [u32; 32] = [
 
 pub fn encode(chars: &[char]) -> Result<String, std::string::FromUtf8Error> {
     // Forward pass: encode to UTF-8, but leave it in 32 bits
-    let fat_utf8 = to_code_points(chars);
+    let mut final_size = 0;
+
+    let fat_utf8: Vec<u32> = chars
+        .iter()
+        .copied()
+        .map(|orig| {
+            let scalar_value: u32 = orig.into();
+            // TODO: invert:
+            let n_bits = (u32::BITS - scalar_value.leading_zeros()) as usize;
+
+            let a = scalar_value & 0b111_000000_000000_000000;
+            let b = scalar_value & 0b000_111111_000000_000000;
+            let c = scalar_value & 0b000_000000_111111_000000;
+            let d = scalar_value & 0b000_000000_000000_111111;
+
+            let encoded = LUT[n_bits] | a << 6 | b << 4 | c << SHIFT_LUT[n_bits] | d;
+            let added_size = utf8_size(encoded);
+            final_size += added_size;
+            encoded
+        })
+        .collect();
 
     // The buffer: starts off uninitialized:
-    let mut buffer = vec![MaybeUninit::uninit(); 4 * fat_utf8.len()];
+    let mut buffer = vec![MaybeUninit::uninit(); final_size];
+
     // We must early return here when empty or else we'll subtract from 0usize which will overflow and panic
     if buffer.is_empty() {
         return Ok(String::new());
     }
 
     // Backwards path:
-    let mut i = buffer.len() - 1;
-    // There's a better way to do this:
-    let mut start = buffer.len();
-    for &encoded in fat_utf8.iter().rev() {
-        let advance = utf8_size(encoded);
-        start -= advance;
-        buffer[i].write((encoded & 0xFF) as u8);
-        buffer[i - 1].write(((encoded & 0xFF00) >> 8) as u8);
-        buffer[i - 2].write(((encoded & 0xFF0000) >> 16) as u8);
-        buffer[i - 3].write(((encoded & 0xFF000000) >> 24) as u8);
-        i = i.saturating_sub(advance);
+    let mut i = buffer.len();
+    let mut reversed_fat_utf8 = fat_utf8.iter().copied().rev();
+    while i > 4 {
+        let encoded = reversed_fat_utf8.next().unwrap();
+        let [a, b, c, d] = encoded.to_be_bytes();
+        buffer[i - 1].write(d);
+        buffer[i - 2].write(c);
+        buffer[i - 3].write(b);
+        buffer[i - 4].write(a);
+        i = i.saturating_sub(utf8_size(encoded));
+    }
+    let last_written_position = i;
+
+    // textbook the last few, this time forwards
+    let mut i = 0;
+    for encoded in fat_utf8.iter().copied() {
+        if i >= last_written_position {
+            break;
+        }
+
+        let byte_size = utf8_size(encoded);
+        let [a, b, c, d] = encoded.to_be_bytes();
+
+        assert!(i + byte_size <= buffer.len());
+        match byte_size {
+            1 => {
+                buffer[i].write(d);
+            }
+            2 => {
+                buffer[i].write(c);
+                buffer[i + 1].write(d);
+            }
+            3 => {
+                buffer[i].write(b);
+                buffer[i + 1].write(c);
+                buffer[i + 2].write(d);
+            }
+            4 => {
+                buffer[i].write(a);
+                buffer[i + 1].write(b);
+                buffer[i + 2].write(c);
+                buffer[i + 3].write(d);
+            }
+            _ => unreachable!(),
+        }
+
+        i += byte_size;
     }
 
     let buffer = unsafe { mem::transmute::<Vec<MaybeUninit<u8>>, Vec<u8>>(buffer) };
-    // Uh: did I not just defeat the point of uniniting a lot of memory?
-    // TODO: use String::from_unchecked() which is another transmute:
-    String::from_utf8(buffer[start..].to_vec())
-}
-
-pub fn encode_one(c: char) -> u32 {
-    let scalar_value: u32 = c.into();
-    // TODO: invert:
-    let n_bits = (u32::BITS - scalar_value.leading_zeros()) as usize;
-
-    let a = scalar_value & 0b1111_000000_000000_000000;
-    let b = scalar_value & 0b0000_111111_000000_000000;
-    let c = scalar_value & 0b0000_000000_111111_000000;
-    let d = scalar_value & 0b0000_000000_000000_111111;
-
-    LUT[n_bits] | a << 6 | b << 4 | c << SHIFT_LUT[n_bits] | d
-}
-
-fn to_code_points(chars: &[char]) -> Vec<u32> {
-    chars.iter().copied().map(encode_one).collect()
+    Ok(unsafe { String::from_utf8_unchecked(buffer) })
 }
 
 fn utf8_size(encoded: u32) -> usize {
