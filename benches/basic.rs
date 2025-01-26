606048b558ae28481644f6de6ccc3425c533adb2
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2025-01-26 18:24:38 +0000

    bench: add 1mb code point test

--- benches/basic.rs
+++ benches/basic.rs
@@ -4,45 +4,55 @@ fn main() {
     divan::main();
 }
 
+#[derive(Clone, Copy, Debug)]
+enum TestCase {
+    Utf8Sample,
+    Test1m,
+}
+
 /// Rust built-in implementation
-#[divan::bench]
-fn built_in_implementation(bencher: Bencher) {
+#[divan::bench(args = [TestCase::Utf8Sample, TestCase::Test1m])]
+fn built_in_implementation(bencher: Bencher, t: TestCase) {
     bencher
-        .with_inputs(|| load_example())
+        .with_inputs(|| t.load_utf32())
         .bench_values(|example| example.into_iter().collect::<String>())
 }
 
 /// "Textbook" implementation -- match/case
-#[divan::bench]
-fn textbook_implementation(bencher: Bencher) {
+#[divan::bench(args = [TestCase::Utf8Sample, TestCase::Test1m])]
+fn textbook_implementation(bencher: Bencher, t: TestCase) {
     use branchless_utf8::implementations::textbook::encode;
 
     bencher
-        .with_inputs(|| load_example())
+        .with_inputs(|| t.load_utf32())
         .bench_values(|example| encode(&example))
 }
 
 /// First version of the branchless implementation:
-#[divan::bench]
-fn naive_implementation(bencher: Bencher) {
+#[divan::bench(args = [TestCase::Utf8Sample, TestCase::Test1m])]
+fn naive_implementation(bencher: Bencher, t: TestCase) {
     use branchless_utf8::implementations::naive_branchless::encode;
 
     bencher
-        .with_inputs(|| load_example())
+        .with_inputs(|| t.load_utf32())
         .bench_values(|example| encode(&example))
 }
 
 /// A better scalar, branchless implementation
-#[divan::bench]
-fn scalar_branchless(bencher: Bencher) {
+#[divan::bench(args = [TestCase::Utf8Sample, TestCase::Test1m])]
+fn scalar_branchless(bencher: Bencher, t: TestCase) {
     use branchless_utf8::implementations::scalar_branchless::encode;
 
     bencher
-        .with_inputs(|| load_example())
+        .with_inputs(|| t.load_utf32())
         .bench_values(|example| encode(&example))
 }
 
-fn load_example() -> Vec<char> {
-    let example = include_str!("utf8-sample.txt");
-    example.chars().collect()
+impl TestCase {
+    fn load_utf32(self) -> Vec<char> {
+        match self {
+            TestCase::Utf8Sample => include_str!("utf8-sample.txt").chars().collect(),
+            TestCase::Test1m => include_str!("test_1m.txt").chars().collect(),
+        }
+    }
 }
