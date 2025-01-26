e9a25ed49ccf3f1fdb5c69545758fa8beb8f8de5
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2025-01-26 11:31:59 +0000

    refactor: clean up module

--- src/implementations/naive_branchless.rs
+++ src/implementations/naive_branchless.rs
@@ -1,5 +1,14 @@
+const LUT: [u32; 22] = [
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xC080, 0xC080, 0xC080, 0xC080, 0xE08080, 0xE08080,
+    0xE08080, 0xE08080, 0xF0808080, 0xF0808080, 0xF0808080, 0xF0808080, 0xF0808080, 0xF0808080,
+];
+
+const SHIFT_LUT: [u32; 32] = [
+    0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+];
+
 pub fn encode_code_points_to_str(chars: &[char]) -> Result<String, std::string::FromUtf8Error> {
-    let fat_utf8 = encode_code_points(chars);
+    let fat_utf8 = to_code_points(chars);
     let mut buffer = vec![0u8; 4 * fat_utf8.len()];
     if buffer.is_empty() {
         return Ok(String::new());
@@ -22,25 +31,7 @@ pub fn encode_code_points_to_str(chars: &[char]) -> Result<String, std::string::
     String::from_utf8(buffer[start..].to_vec())
 }
 
-fn utf8_size(encoded: u32) -> usize {
-    std::cmp::max(1, (u32::BITS - encoded.leading_zeros()) as usize / 8)
-}
-
-pub fn encode_code_points(chars: &[char]) -> Vec<u32> {
-    chars.iter().copied().map(encode_one).collect()
-}
-
 pub fn encode_one(c: char) -> u32 {
-    const LUT: [u32; 22] = [
-        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xC080, 0xC080, 0xC080, 0xC080, 0xE08080, 0xE08080,
-        0xE08080, 0xE08080, 0xF0808080, 0xF0808080, 0xF0808080, 0xF0808080, 0xF0808080, 0xF0808080,
-    ];
-
-    const SHIFT_LUT: [u32; 32] = [
-        0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 2,
-    ];
-
     let scalar_value: u32 = c.into();
     // TODO: invert:
     let n_bits = (u32::BITS - scalar_value.leading_zeros()) as usize;
@@ -51,4 +42,12 @@ pub fn encode_one(c: char) -> u32 {
     let d = scalar_value & 0b0000_000000_000000_111111;
 
     LUT[n_bits] | a << 6 | b << 4 | c << SHIFT_LUT[n_bits] | d
+}
+
+fn to_code_points(chars: &[char]) -> Vec<u32> {
+    chars.iter().copied().map(encode_one).collect()
+}
+
+fn utf8_size(encoded: u32) -> usize {
+    std::cmp::max(1, (u32::BITS - encoded.leading_zeros()) as usize / 8)
 }
