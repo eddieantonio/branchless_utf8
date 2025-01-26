c7bae78a07ed42129f0e60de5721296c851c1252
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2025-01-26 12:16:13 +0000

    bench: built-in implementation

--- benches/basic.rs
+++ benches/basic.rs
@@ -10,7 +10,14 @@ fn naive_implementation(bencher: Bencher) {
 
     bencher
         .with_inputs(|| load_example())
-        .bench_values(|s| encode(&s))
+        .bench_values(|example| encode(&example))
+}
+
+#[divan::bench]
+fn built_in_implementation(bencher: Bencher) {
+    bencher
+        .with_inputs(|| load_example())
+        .bench_values(|example| example.into_iter().collect::<String>())
 }
 
 fn load_example() -> Vec<char> {
